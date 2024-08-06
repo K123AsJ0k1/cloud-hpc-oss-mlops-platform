5e239fb5980b59b2f54d6ca666f361b1e94b8ecb
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2024-08-06 21:47:01 +0300

    Forwarder frontend commenting

--- applications/article/forwarder/frontend/run_fastapi.py
+++ applications/article/forwarder/frontend/run_fastapi.py
@@ -4,9 +4,9 @@ import warnings
 # Make run with Python3 run_fastapi.py
 warnings.filterwarnings("ignore")
 if __name__ == "__main__":
-    os.environ['REDIS_ENDPOINT'] = '127.0.0.1'
-    os.environ['REDIS_PORT'] = '6379'
-    os.environ['REDIS_DB'] = '0'
+    #os.environ['REDIS_ENDPOINT'] = '127.0.0.1'
+    #os.environ['REDIS_PORT'] = '6379'
+    #os.environ['REDIS_DB'] = '0'
 
     from setup_fastapi import setup_fastapi_app
 
