caa62f4e5bce086758c9cb3393a2ea43bd95b203
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2024-08-07 14:04:38 +0300

    Working submitter

--- applications/article/submitter/backend/run_celery.py
+++ applications/article/submitter/backend/run_celery.py
@@ -4,17 +4,17 @@ import warnings
 # Make run with Python3 run_celery.py
 warnings.filterwarnings("ignore") 
 if __name__ == '__main__': 
-    os.environ['REDIS_ENDPOINT'] = '127.0.0.1'
-    os.environ['REDIS_PORT'] = '6379'
-    os.environ['REDIS_DB'] = '0'
+    #os.environ['REDIS_ENDPOINT'] = '127.0.0.1'
+    #os.environ['REDIS_PORT'] = '6379'
+    #os.environ['REDIS_DB'] = '0'
 
-    os.environ['CELERY_CONCURRENCY'] = '8'
-    os.environ['CELERY_LOGLEVEL'] = 'warning'
+    #os.environ['CELERY_CONCURRENCY'] = '8'
+    #os.environ['CELERY_LOGLEVEL'] = 'warning'
     
-    os.environ['FLOWER_ENDPOINT'] = '127.0.0.1'
-    os.environ['FLOWER_PORT'] = '6601'
-    os.environ['FLOWER_USERNAME'] = 'flower123'
-    os.environ['FLOWER_PASSWORD'] = 'flower456'
+    #os.environ['FLOWER_ENDPOINT'] = '127.0.0.1'
+    #os.environ['FLOWER_PORT'] = '6601'
+    #os.environ['FLOWER_USERNAME'] = 'flower123'
+    #os.environ['FLOWER_PASSWORD'] = 'flower456'
 
     from setup_celery import setup_celery_app
     
