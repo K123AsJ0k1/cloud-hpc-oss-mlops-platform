fdcd2c92be44524987325e4f58b5d1d8ecc45555
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2024-08-08 13:30:14 +0300

    Boilerplate refactoring

--- applications/article/pipeline/boilerplate/platforms/ray/artifacts.py
+++ applications/article/pipeline/boilerplate/platforms/ray/artifacts.py
@@ -1,111 +1,111 @@
 def gather_ray_artifacts(
-        logger: any,
-        allas_client: any,
-        allas_bucket: str,
-        kubeflow_user: str,
-        ray_parameters: any
-    ):
-        collected_parameters = {}
-        collected_metrics = {}
-        collected_artifacts = {}    
-        job_parameters = ray_parameters['job-parameters']
+    logger: any,
+    allas_client: any,
+    allas_bucket: str,
+    kubeflow_user: str,
+    ray_parameters: any
+):
+    collected_parameters = {}
+    collected_metrics = {}
+    collected_artifacts = {}    
+    job_parameters = ray_parameters['job-parameters']
 
-        logger.info('Training hyperparameters:')
-        
-        for key,value in job_parameters.items():
-            if 'hp-' in key:
-                formatted_key = key.replace('hp-', '')
-                logger.info(str(key) + '=' + str(value))
-                collected_parameters[formatted_key] = value
+    logger.info('Training hyperparameters:')
+    
+    for key,value in job_parameters.items():
+        if 'hp-' in key:
+            formatted_key = key.replace('hp-', '')
+            logger.info(str(key) + '=' + str(value))
+            collected_parameters[formatted_key] = value
 
+    logger.info('')
+    parameters_path = job_parameters['artifact-path'] + '-parameters'
+    logger.info('Used parameters path: ' + str(parameters_path))
+    ray_job_artifact_parameters = get_object(
+        client = allas_client,
+        bucket_name = allas_bucket,
+        object_path = parameters_path
+    )
+    
+    if not ray_job_artifact_parameters is None:
+        collected_artifacts['parameters'] = ray_job_artifact_parameters
+
+    predictions_path = job_parameters['artifact-path'] + '-predictions'
+    logger.info('Used predictions path: ' + str(predictions_path))
+    ray_job_artifact_predictions = get_object(
+        client = allas_client,
+        bucket_name = allas_bucket,
+        object_path = predictions_path
+    )
+
+    if not ray_job_artifact_predictions is None:
+        collected_artifacts['predictions'] = ray_job_artifact_predictions
+
+    metrics_path = job_parameters['artifact-path'] + '-metrics'
+    logger.info('Used metrics path: ' + str(metrics_path))
+    ray_job_artifact_metrics = get_object(
+        client = allas_client,
+        bucket_name = allas_bucket,
+        object_path = metrics_path
+    )
+
+    if not ray_job_artifact_metrics is None:
+        performance = ray_job_artifact_metrics['performance']
         logger.info('')
-        parameters_path = job_parameters['artifact-path'] + '-parameters'
-        logger.info('Used parameters path: ' + str(parameters_path))
-        ray_job_artifact_parameters = get_object(
-            client = allas_client,
-            bucket_name = allas_bucket,
-            object_path = parameters_path
-        )
-        
-        if not ray_job_artifact_parameters is None:
-            collected_artifacts['parameters'] = ray_job_artifact_parameters
+        logger.info('Training metrics:')
+        for key,value in performance.items():
+            if key == 'name':
+                continue
+            if 'class-' in key:
+                image_labels = {
+                    0: 'top',
+                    1: 'trouser',
+                    2: 'pullover',
+                    3: 'dress',
+                    4: 'coat',
+                    5: 'sandal',
+                    6: 'shirt',
+                    7: 'sneaker',
+                    8: 'bag',
+                    9: 'ankle-boot',
+                }
+                logger.info('')
+                i = 0
+                for class_value in value:
+                    formatted_key = key.replace('class', image_labels[i])
+                    rounded_value = round(class_value,5)
+                    logger.info(str(formatted_key) + '=' + str(rounded_value))
+                    collected_metrics[formatted_key] = rounded_value
+                    i += 1
+                continue
+            rounded_value = round(value,5)
+            logger.info(str(key) + '=' + str(rounded_value))
+            collected_metrics[key] = rounded_value
 
-        predictions_path = job_parameters['artifact-path'] + '-predictions'
-        logger.info('Used predictions path: ' + str(predictions_path))
-        ray_job_artifact_predictions = get_object(
-            client = allas_client,
-            bucket_name = allas_bucket,
-            object_path = predictions_path
-        )
-
-        if not ray_job_artifact_predictions is None:
-            collected_artifacts['predictions'] = ray_job_artifact_predictions
-
-        metrics_path = job_parameters['artifact-path'] + '-metrics'
-        logger.info('Used metrics path: ' + str(metrics_path))
-        ray_job_artifact_metrics = get_object(
-            client = allas_client,
-            bucket_name = allas_bucket,
-            object_path = metrics_path
-        )
-
-        if not ray_job_artifact_metrics is None:
-            performance = ray_job_artifact_metrics['performance']
-            logger.info('')
-            logger.info('Training metrics:')
-            for key,value in performance.items():
-                if key == 'name':
-                    continue
-                if 'class-' in key:
-                    image_labels = {
-                        0: 'top',
-                        1: 'trouser',
-                        2: 'pullover',
-                        3: 'dress',
-                        4: 'coat',
-                        5: 'sandal',
-                        6: 'shirt',
-                        7: 'sneaker',
-                        8: 'bag',
-                        9: 'ankle-boot',
-                    }
-                    logger.info('')
-                    i = 0
-                    for class_value in value:
-                        formatted_key = key.replace('class', image_labels[i])
-                        rounded_value = round(class_value,5)
-                        logger.info(str(formatted_key) + '=' + str(rounded_value))
-                        collected_metrics[formatted_key] = rounded_value
-                        i += 1
-                    continue
-                rounded_value = round(value,5)
-                logger.info(str(key) + '=' + str(rounded_value))
-                collected_metrics[key] = rounded_value
-
+    logger.info('')
+    time_path = job_parameters['time-folder-path'] + '/ray-job'
+    logger.info('Used time path: ' + str(time_path))
+    ray_job_times = get_object(
+        client = allas_client,
+        bucket_name = allas_bucket,
+        object_path = time_path
+    )
+    if not ray_job_times is None:
         logger.info('')
-        time_path = job_parameters['time-folder-path'] + '/ray-job'
-        logger.info('Used time path: ' + str(time_path))
-        ray_job_times = get_object(
-            client = allas_client,
-            bucket_name = allas_bucket,
-            object_path = time_path
-        )
-        if not ray_job_times is None:
-            logger.info('')
-            logger.info('Training times:')
-            user_ray_job_times = ray_job_times[kubeflow_user]
-            for i in range(0,2):
-                object_key = len(user_ray_job_times) - i
-                time_object = user_ray_job_times[str(object_key)]
-                formatted_key = time_object['name'] + '-total-seconds'
-                value = time_object['total-seconds']
-                collected_metrics[formatted_key] = value
-                logger.info(str(formatted_key) + '=' + str(value))
-        
-        artifacts = [
-            collected_parameters, 
-            collected_metrics,
-            collected_artifacts
-        ]
+        logger.info('Training times:')
+        user_ray_job_times = ray_job_times[kubeflow_user]
+        for i in range(0,2):
+            object_key = len(user_ray_job_times) - i
+            time_object = user_ray_job_times[str(object_key)]
+            formatted_key = time_object['name'] + '-total-seconds'
+            value = time_object['total-seconds']
+            collected_metrics[formatted_key] = value
+            logger.info(str(formatted_key) + '=' + str(value))
+    
+    artifacts = [
+        collected_parameters, 
+        collected_metrics,
+        collected_artifacts
+    ]
 
-        return artifacts
+    return artifacts
--- applications/article/pipeline/boilerplate/platforms/slurm/parse.py
+++ applications/article/pipeline/boilerplate/platforms/slurm/parse.py
@@ -1,105 +1,198 @@
-def gather_slurm_artifacts(
-        logger: any,
-        proxy_url: str,
-        proxy_target: str,
-        kubeflow_user: str,
-        slurm_job_id: str
-    ) -> any:
-        collected_parameters = {}
-        collected_metrics = {}
-        
-        logger.info('Collecting SLURM artifacts')
-        seff,sacct,logs = get_job_artifacts(
-            proxy_url = proxy_url,
-            kubeflow_user = kubeflow_user,
-            slurm_job_id = slurm_job_id,
-            target = proxy_target
-        )
-
+def parse_job_sacct(
+    logger: any,
+    sacct: any
+) -> any:
+    collected_parameters = {}
+    collected_metrics = {}
+    logger.info('')
+    logger.info('Sacct:')
+    for row in sacct.keys():
+        logger.info('Row ' + str(row))
+        row_metadata = sacct[row]['metadata']
+        row_metrics = sacct[row]['metrics']
+    
         relevant_metadata = [
-            'billed-project',
-            'cluster',
-            'status'
+            'job-name',
+            'state'
         ]
         
-        if not seff is None:
-            logger.info('')
-            logger.info('Seff:')
-            seff_metadata = seff['metadata']
-            for key,value in seff_metadata.items():
+        for key, value in row_metadata.items():
+            if key in relevant_metadata:
+                logger.info(str(key) + '=' + str(value))
+                collected_parameters[str(row) + '-' + key] = value
+    
+        relevant_metrics = [
+            'ave-cpu-seconds',
+            'cpu-time-seconds',
+            'elapsed-seconds',
+            'total-cpu-seconds',
+        ]
+        
+        for key, value in row_metrics.items():
+            if key in relevant_metrics:
+                logger.info(str(key) + '=' + str(value))
+                collected_metrics[str(row) + '-' + key] = value
+    
+        start_time = row_metrics['start-time']
+        submit_time = row_metrics['submit-time']
+        end_time = row_metrics['end-time']
+    
+        submit_date = datetime.fromtimestamp(start_time).strftime('%Y-%m-%d-%H:%M:%S')
+        total_start_seconds = (submit_time-start_time)
+        total_end_seconds = (end_time-submit_time)
+    
+        logger.info('submit-date=' + str(submit_date))
+        collected_parameters[str(row) + '-submit-date'] = submit_date
+        
+        logger.info('total-submit-start-seconds=' + str(total_start_seconds))
+        collected_metrics[str(row) + '-total-submit-start-seconds'] = total_start_seconds
+        
+        logger.info('total-start-end-seconds=' + str(total_end_seconds))
+        collected_metrics[str(row) + '-total-start-end-seconds'] = total_end_seconds
+        
+        logger.info('')
+    return collected_parameters, collected_metrics
+
+def parse_job_seff(
+    logger: any,
+    seff: any
+) -> any:
+    collected_parameters = {}
+    collected_metrics = {}
+    relevant_metadata = [
+        'billed-project',
+        'cluster',
+        'status'
+    ]
+
+    logger.info('')
+    logger.info('Seff:')
+    seff_metadata = seff['metadata']
+    for key,value in seff_metadata.items():
+        if key in relevant_metadata:
+            logger.info(str(key) + '=' + str(value))
+            collected_parameters[key] = value
+    
+    relevant_metrics = [
+        'billing-units',
+        'cpu-efficiency-percentage',
+        'cpu-efficiency-seconds',
+        'cpu-utilized-seconds',
+        'job-wall-clock-time-seconds',
+        'memory-efficiency-percentage'
+    ]
+
+
+    seff_metrics = seff['metrics']
+    for key,value in seff_metrics.items():
+        if key in relevant_metrics:
+            logger.info(str(key) + '=' + str(value))
+            collected_metrics[key] = value
+
+    return collected_parameters, collected_metrics
+
+def gather_slurm_artifacts(
+    logger: any,
+    proxy_url: str,
+    proxy_target: str,
+    kubeflow_user: str,
+    slurm_job_id: str
+) -> any:
+    collected_parameters = {}
+    collected_metrics = {}
+    
+    logger.info('Collecting SLURM artifacts')
+    seff,sacct,logs = get_job_artifacts(
+        proxy_url = proxy_url,
+        kubeflow_user = kubeflow_user,
+        slurm_job_id = slurm_job_id,
+        target = proxy_target
+    )
+
+    relevant_metadata = [
+        'billed-project',
+        'cluster',
+        'status'
+    ]
+    
+    if not seff is None:
+        logger.info('')
+        logger.info('Seff:')
+        seff_metadata = seff['metadata']
+        for key,value in seff_metadata.items():
+            if key in relevant_metadata:
+                logger.info(str(key) + '=' + str(value))
+                collected_parameters[key] = value
+        
+        relevant_metrics = [
+            'billing-units',
+            'cpu-efficiency-percentage',
+            'cpu-efficiency-seconds',
+            'cpu-utilized-seconds',
+            'job-wall-clock-time-seconds',
+            'memory-efficiency-percentage'
+        ]
+    
+
+        seff_metrics = seff['metrics']
+        for key,value in seff_metrics.items():
+            if key in relevant_metrics:
+                logger.info(str(key) + '=' + str(value))
+                collected_metrics[key] = value
+    
+    if not sacct is None:            
+        logger.info('')
+        logger.info('Sacct:')
+        for row in sacct.keys():
+            logger.info('Row ' + str(row))
+            row_metadata = sacct[row]['metadata']
+            row_metrics = sacct[row]['metrics']
+        
+            relevant_metadata = [
+                'job-name',
+                'state'
+            ]
+            
+            for key, value in row_metadata.items():
                 if key in relevant_metadata:
                     logger.info(str(key) + '=' + str(value))
-                    collected_parameters[key] = value
-            
-            relevant_metrics = [
-                'billing-units',
-                'cpu-efficiency-percentage',
-                'cpu-efficiency-seconds',
-                'cpu-utilized-seconds',
-                'job-wall-clock-time-seconds',
-                'memory-efficiency-percentage'
-            ]
+                    collected_parameters[str(row) + '-' + key] = value
         
-
-            seff_metrics = seff['metrics']
-            for key,value in seff_metrics.items():
+            relevant_metrics = [
+                'ave-cpu-seconds',
+                'cpu-time-seconds',
+                'elapsed-seconds',
+                'total-cpu-seconds',
+            ]
+            
+            for key, value in row_metrics.items():
                 if key in relevant_metrics:
                     logger.info(str(key) + '=' + str(value))
-                    collected_metrics[key] = value
+                    collected_metrics[str(row) + '-' + key] = value
         
-        if not sacct is None:            
+            start_time = row_metrics['start-time']
+            submit_time = row_metrics['submit-time']
+            end_time = row_metrics['end-time']
+        
+            submit_date = datetime.fromtimestamp(start_time).strftime('%Y-%m-%d-%H:%M:%S')
+            total_start_seconds = (submit_time-start_time)
+            total_end_seconds = (end_time-submit_time)
+        
+            logger.info('submit-date=' + str(submit_date))
+            collected_parameters[str(row) + '-submit-date'] = submit_date
+            
+            logger.info('total-submit-start-seconds=' + str(total_start_seconds))
+            collected_metrics[str(row) + '-total-submit-start-seconds'] = total_start_seconds
+            
+            logger.info('total-start-end-seconds=' + str(total_end_seconds))
+            collected_metrics[str(row) + '-total-start-end-seconds'] = total_end_seconds
+            
             logger.info('')
-            logger.info('Sacct:')
-            for row in sacct.keys():
-                logger.info('Row ' + str(row))
-                row_metadata = sacct[row]['metadata']
-                row_metrics = sacct[row]['metrics']
-            
-                relevant_metadata = [
-                    'job-name',
-                    'state'
-                ]
-                
-                for key, value in row_metadata.items():
-                    if key in relevant_metadata:
-                        logger.info(str(key) + '=' + str(value))
-                        collected_parameters[str(row) + '-' + key] = value
-            
-                relevant_metrics = [
-                    'ave-cpu-seconds',
-                    'cpu-time-seconds',
-                    'elapsed-seconds',
-                    'total-cpu-seconds',
-                ]
-                
-                for key, value in row_metrics.items():
-                    if key in relevant_metrics:
-                        logger.info(str(key) + '=' + str(value))
-                        collected_metrics[str(row) + '-' + key] = value
-            
-                start_time = row_metrics['start-time']
-                submit_time = row_metrics['submit-time']
-                end_time = row_metrics['end-time']
-            
-                submit_date = datetime.fromtimestamp(start_time).strftime('%Y-%m-%d-%H:%M:%S')
-                total_start_seconds = (submit_time-start_time)
-                total_end_seconds = (end_time-submit_time)
-            
-                logger.info('submit-date=' + str(submit_date))
-                collected_parameters[str(row) + '-submit-date'] = submit_date
-                
-                logger.info('total-submit-start-seconds=' + str(total_start_seconds))
-                collected_metrics[str(row) + '-total-submit-start-seconds'] = total_start_seconds
-                
-                logger.info('total-start-end-seconds=' + str(total_end_seconds))
-                collected_metrics[str(row) + '-total-start-end-seconds'] = total_end_seconds
-                
-                logger.info('')
-            
-        artifacts = [
-            collected_parameters, 
-            collected_metrics,
-            logs
-        ]
+        
+    artifacts = [
+        collected_parameters, 
+        collected_metrics,
+        logs
+    ]
 
-        return artifacts
+    return artifacts
