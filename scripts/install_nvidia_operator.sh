80e3f0d77bea0d2444ee9b946cafe5d927225fe9
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2024-08-20 12:24:06 +0300

    Initial kind GPU support

---
+++ scripts/install_nvidia_operator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eo pipefail
+
+helm repo add nvidia https://helm.ngc.nvidia.com/nvidia || true
+helm repo update
+
+helm install --wait --generate-name \
+     -n gpu-operator --create-namespace \
+     nvidia/gpu-operator --set driver.enabled=false
