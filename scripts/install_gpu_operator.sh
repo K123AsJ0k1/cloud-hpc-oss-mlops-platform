4e24a729746dc86a3a0a6aedbd8c9caa07874dd6
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2024-08-26 13:11:00 +0300

    Script fixes

--- scripts/install_gpu_operator.sh
+++ scripts/install_gpu_operator.sh
@@ -2,9 +2,10 @@
 
 set -eo pipefail
 
-helm repo add nvidia https://helm.ngc.nvidia.com/nvidia || true
+helm repo add nvidia https://helm.ngc.nvidia.com/nvidia
 helm repo update
 
 helm install --wait --generate-name \
      -n gpu-operator --create-namespace \
-     nvidia/gpu-operator --set driver.enabled=false 
+     nvidia/gpu-operator \
+     --set driver.enabled=false 
